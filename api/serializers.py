f2325d10d326734815bc78ab4b08340e7aac4143
hirokikotera13456 <hirokikotera0211@gmail.com>
2025-03-18 14:02:40 +0900

    section35まで

---
+++ api/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+
+class ItemSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=20)
+    price = serializers.IntegerField(min_value=0)
+    discounted_price = serializers.IntegerField(min_value=0)
+
+    def validate_price(self, value):
+        print(value)
+        if value % 10 != 0:
+            raise serializers.ValidationError("1桁目は０にして下さい")
+        return value
+
+    def validate_name(self, value):
+        print({value})
+        if value[0].islower():
+            raise serializers.ValidationError("最初の文字は大文字にして下さい")
+        return value
+
+    def validate(self, data):
+        print(data)
+        price = data.get("price")
+        discounted_price = data.get("discounted_price")
+        if price < discounted_price:
+            raise serializers.ValidationError(
+                "割引価格は本来の価格以下の値にして下さい"
+            )
+        return data
--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ItemSerializer
 
 # Create your views here.
+
+
+class ItemView(APIView):
+    def get(self, request):
+        return Response({"method": "get"})
+
+    def post(self, request):
+        print(request.data)
+        serializer = ItemSerializer(data=request.data)
+        print(serializer)
+        print(serializer.is_valid(raise_exception=True))
+        print(serializer.errors)
+        return Response({"method": "post"})
+
+    def delete(self, request):
+        return Response({"method": "get"})
+
+    def patch(self, request):
+        return Response({"method": "get"})
